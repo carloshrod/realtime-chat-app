8b0981574bc01b120277c1b1502cf90072e18c71
chrod <chrod.dev@gmail.com>
2024-02-29 22:11:11 -0500

    Add Header component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,9 @@
+import Header from '../components/Header';
+
 export default function Home() {
 	return (
-		<main
-			className={`flex min-h-screen flex-col items-center justify-between p-24`}
-		>
-			<h1 className='text-3xl font-bold'>Chat App</h1>
+		<main>
+			<Header />
 		</main>
 	);
 }
